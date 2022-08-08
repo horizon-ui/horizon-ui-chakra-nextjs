84341f0fdb3856fb873ec4e2d14eba476de503b8
simmmpleweb <90040741+simmmpleweb@users.noreply.github.com>
2022-08-08 13:12:58 +0300

    Icons

---
+++ src/components/icons/IconBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+
+export default function IconBox(props: any) {
+	const { icon, ...rest } = props;
+
+	return (
+		<Flex alignItems={'center'} justifyContent={'center'} borderRadius={'50%'} {...rest}>
+			{icon}
+		</Flex>
+	);
+}
