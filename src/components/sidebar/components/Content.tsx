7de16b44486840acc49ada891238d624d7a66d85
simmmpleweb <90040741+simmmpleweb@users.noreply.github.com>
2022-09-13 17:10:32 +0300

    Scrollbar solved

--- src/components/sidebar/components/Content.tsx
+++ src/components/sidebar/components/Content.tsx
@@ -1,40 +1,34 @@
 // chakra imports
-import { Box, Flex, Stack } from '@chakra-ui/react'
+import { Box, Flex, Stack } from '@chakra-ui/react';
 //   Custom components
-import Brand from 'components/sidebar/components/Brand'
-import Links from 'components/sidebar/components/Links'
-import SidebarCard from 'components/sidebar/components/SidebarCard'
-import { IRoute } from 'types/navigation'
+import Brand from 'components/sidebar/components/Brand';
+import Links from 'components/sidebar/components/Links';
+import SidebarCard from 'components/sidebar/components/SidebarCard';
+import { IRoute } from 'types/navigation';
 
 // FUNCTIONS
 
 interface SidebarContentProps {
-  routes: IRoute[]
+	routes: IRoute[];
 }
 
-function SidebarContent (props: SidebarContentProps) {
-  const { routes } = props
-  // SIDEBAR
-  return (
-    <Flex direction='column' height='100%' pt='25px' borderRadius='30px'>
-      <Brand />
-      <Stack direction='column' mb='auto' mt='8px'>
-        <Box ps='20px' pe={{ md: '16px', '2xl': '1px' }}>
-          <Links routes={routes} />
-        </Box>
-      </Stack>
+function SidebarContent(props: SidebarContentProps) {
+	const { routes } = props;
+	// SIDEBAR
+	return (
+		<Flex direction='column' height='100%' pt='25px' borderRadius='30px'>
+			<Brand />
+			<Stack direction='column' mt='8px' mb='auto'>
+				<Box ps='20px' pe={{ lg: '16px', '2xl': '16px' }}>
+					<Links routes={routes} />
+				</Box>
+			</Stack>
 
-      <Box
-        ps='20px'
-        pe={{ md: '16px', '2xl': '0px' }}
-        mt='60px'
-        mb='40px'
-        borderRadius='30px'
-      >
-        <SidebarCard />
-      </Box>
-    </Flex>
-  )
+			<Box ps='20px' pe={{ lg: '16px', '2xl': '20px' }} mt='60px' mb='40px' borderRadius='30px'>
+				<SidebarCard />
+			</Box>
+		</Flex>
+	);
 }
 
-export default SidebarContent
+export default SidebarContent;
