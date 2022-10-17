0819840996ef35bf2bc65506ad83a03b58edc84e
jbric16 <jbric16@gmail.com>
2022-10-16 17:34:05 -0700

    Set up prefix and change to Google fonts

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,15 +1,33 @@
-import { Box, ChakraComponent } from '@chakra-ui/react'
-import * as React from 'react'
-import NextImage from 'next/image'
-import { ComponentProps } from 'react'
+import { Box, ChakraComponent } from "@chakra-ui/react";
+import * as React from "react";
+import NextImage from "next/image";
+import { ComponentProps } from "react";
 
-interface ImageProps extends ComponentProps<ChakraComponent<'div', {}>> {}
+interface ImageProps extends ComponentProps<ChakraComponent<"div", {}>> {}
+
+function parseAssetPrefix(image: string) {
+  const alreadyHasHttp = image.match("http");
+  if (alreadyHasHttp) return image;
+
+  const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
+  const alreadyHasPrefix = image.match(prefix);
+
+  const finalUrl = alreadyHasPrefix ? image : `${prefix}${image}`;
+  return finalUrl;
+}
 
 export const Image = (props: ImageProps) => {
-  const { src, alt, ...rest } = props
+  const { src, alt, ...rest } = props;
+
+  const imageUrl = typeof src === "string" ? src : src?.src;
   return (
-    <Box overflow={'hidden'} position='relative' {...rest}>
-      <NextImage objectFit='cover' layout='fill' src={src} alt={alt} />
+    <Box overflow={"hidden"} position="relative" {...rest}>
+      <NextImage
+        objectFit="cover"
+        layout="fill"
+        src={parseAssetPrefix(imageUrl)}
+        alt={alt}
+      />
     </Box>
-  )
-}
+  );
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document () {
+export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link rel='apple-touch-icon' href='/logo192.png' />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="icon"
+          href={(process.env.NEXT_PUBLIC_BASE_PATH || "") + "/favicon.ico"}
+        />
       </Head>
-      <body id='root'>
+      <body id="root">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
