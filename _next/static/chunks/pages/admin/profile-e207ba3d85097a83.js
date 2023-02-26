a2dece91b5905ab5159234bb61c8f20f9fbc3004
simmmpleweb <90040741+simmmpleweb@users.noreply.github.com>
2023-02-26 16:53:10 +0000

    Deploying to gh-pages from  @ dafc12743c033dbef1f204566ebb004d104bc5c6 🚀

---
+++ _next/static/chunks/pages/admin/profile-e207ba3d85097a83.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[172],{4745:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/profile",function(){return u(6809)}])}},function(n){n.O(0,[228,445,937,866,994,260,280,101,241,336,774,888,179],function(){return n(n.s=4745)}),_N_E=n.O()}]);
