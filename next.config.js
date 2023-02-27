eb9b3591bc694f20f1316068d433e5d6d639a6ec
simmmpleweb <hello@simmmple.com>
2023-02-27 15:32:18 +0200

    Next config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,18 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: '/horizon-ui-chakra-nextjs',
+  async redirects() {
+    return [ 
+      {
+        source: "/",
+        destination: "/horizon-ui-chakra-nextjs/admin/default",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
