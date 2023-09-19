c6014d8802b67bebaebc31c27fa0cd95d69f3db6
LevinChun <96810516+LevinChun@users.noreply.github.com>
2023-09-20 01:11:17 +0700

    Update index.tsx

--- src/layouts/admin/index.tsx
+++ src/layouts/admin/index.tsx
@@ -58,7 +58,7 @@ export default function AdminLayout (props: DashboardLayoutProps) {
             <Box>
               <Navbar
                 onOpen={onOpen}
-                logoText={'Horizon UI Dashboard PRO'}
+                logoText={'Horizon UI Dashboard PROO'}
                 brandText={getActiveRoute(routes)}
                 secondary={getActiveNavbar(routes)}
                 message={getActiveNavbarText(routes)}
